64d2dcf6358d4e8d32a5ff70778af7ef95bf4967
aulakhsahib <sahibaulakh2003@gmail.com>
2024-03-01 20:12:28 -0700

    added state for todo list

---
+++ App.jsx
@@ -0,0 +1,34 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React from 'react';
+import {useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  Button,
+} from 'react-native';
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
+
+function App() {
+  const [tasks, setTasks] = useState(['Do laundry', 'Go to gym', 'Walk dog']);
+
+  return (
+    <SafeAreaView>
+      <ToDoList tasks={tasks} />
+
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ ToDoList.jsx
@@ -0,0 +1,31 @@
+import {ScrollView, StyleSheet, Pressable, View, Text} from 'react-native';
+
+export default function ToDoList({tasks}) {
+  return (
+    <>
+      <ScrollView>
+        {tasks.map((task, index) => (
+          <Pressable key={index}>
+            <View style={[styles.task]}>
+              <Text style={styles.taskText}>{task}</Text>
+            </View>
+          </Pressable>
+        ))}
+      </ScrollView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  completed: {
+    backgroundColor: '#e0e0e0',
+  },
+  taskText: {
+    fontSize: 16,
+  },
+});
